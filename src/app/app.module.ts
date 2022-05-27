ba18a95cf8aed341f6ae1f26f37c7d0cd2e9dc74
GabrielMSosa <86896491+GabrielMSosa@users.noreply.github.com>
2022-05-27 18:11:12 -0300

    commitaso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgxScannerQrcodeModule } from 'ngx-scanner-qrcode';
 import { AppComponent } from './app.component';
+import { QrlectorComponent } from './components/qrlector/qrlector.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QrlectorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxScannerQrcodeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/qrlector/qrlector.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-qrlector',
+  templateUrl: './qrlector.component.html',
+  styleUrls: ['./qrlector.component.css']
+})
+export class QrlectorComponent implements OnInit {
+output:any;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
